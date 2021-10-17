2a305774b13b8cf5cbe27729957b54189d466ac1
Felipe Mohr <felipe18mohr@gmail.com>
2021-10-17 02:38:45 -0300

    Create State Machine diagram

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
-#include <mbed.h>
+#include "mbed.h"
+#include "TextLCD.h"
+
+Serial pc(PA_9, PA_10);
 
 DigitalOut led(PC_13);
+AnalogIn pot1(PA_6);
+AnalogIn pot2(PA_2);
 
-int main()
-{
-  led = 1;
-  while (1)
+int main() {
+
+  led = 0;
+  while(1)
   {
     led = !led;
-    wait_ms(100);
+    pc.printf("Pot 1 = %.2f \n", pot1.read()); 
+    pc.printf("Pot 2 = %.2f \n\n", pot2.read()); 
+    wait_ms(500);
   }
-  
 }
